caf80c4c90fb34ff56617febb517b70cc02664a0
Dominic Ifechuku <dominicdutchboy@gmail.com>
2025-08-09 20:08:40 -0700

    temporarily redacted mongoose model

--- src/store/userModel.ts
+++ src/store/userModel.ts
@@ -1,9 +1,9 @@
-// User.ts
-import mongoose from 'mongoose';
+// // User.ts
+// import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  telegramId: { type: Number, required: true, unique: true },
-  email: { type: String, required: true },
-});
+// const userSchema = new mongoose.Schema({
+//   telegramId: { type: Number, required: true, unique: true },
+//   email: { type: String, required: true },
+// });
 
-export const User = mongoose.model('User', userSchema);
+// export const User = mongoose.model('User', userSchema);
