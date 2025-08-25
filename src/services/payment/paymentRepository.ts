1e39a4072f8db880221579322037619c0840364c
Dominic Ifechuku <dominicdutchboy@gmail.com>
2025-08-25 02:19:36 -0700

    chore: impl proper structured logging with pino and refactored payment service to use logger

--- src/services/payment/paymentRepository.ts
+++ src/services/payment/paymentRepository.ts
@@ -1,4 +1,5 @@
 import { DataSource, Repository } from "typeorm";
+import pino from "pino";
 import { PostgresService } from "../../internal/store/database";
 import { Transaction } from "../../internal/store/entities/TransactionEntity";
 import { NotFoundErrror } from "../../internal/error";
@@ -34,55 +35,84 @@ export interface IPaymentRepository {
 }
 
 export class PaymentRepository implements IPaymentRepository {
+    private logs: pino.Logger;
     private paymentRepository: Repository<Transaction>;
     private dataSource: DataSource;
 
-    constructor(postgresService: PostgresService, dataSource: DataSource){
+    constructor(postgresService: PostgresService, dataSource: DataSource, logger: pino.Logger) {
         /* get repository from injected postgresService */
         this.paymentRepository = postgresService.getPaymentRepository();
         this.dataSource = dataSource;
+        this.logs = logger;
     }
 
     /* create new payment intent */
     async createTransaction(data: Partial<ITransaction>): Promise<Transaction> {
-        const paymentIntent = this.paymentRepository.create(data);
-        const savedPayment = await this.paymentRepository.save(paymentIntent);
-        return savedPayment;
+        try {
+            const newTransaction = this.paymentRepository.create(data);
+            const savedTransaction = await this.paymentRepository.save(newTransaction);
+            this.logs.info('Transaction record created successfully...');
+            return savedTransaction;
+        } catch (e) {
+            this.logs.error(e, 'Error creating transaction record...');
+            throw e;
+        }
     }
 
     /* updates transaction status after verification */
     async updateTransactionStatus(reference: string, status: ITransaction['status']): Promise<Transaction> {
-        /*  Start database transaction */
-        return await this.dataSource.transaction(async (transactionEntityManager) => {
-            const paymentRepo = transactionEntityManager.getRepository(Transaction);
-            const payment = await paymentRepo.findOne({
-                where: { reference }
+       try {
+         /*  Start database transaction */
+            return await this.dataSource.transaction(async (transactionEntityManager) => {
+                const paymentRepo = transactionEntityManager.getRepository(Transaction);
+                const payment = await paymentRepo.findOne({
+                    where: { reference }
+                });
+
+                if (!payment) {
+                    this.logs.error('Transaction record not found...[CATCH_PAYMENT_REPOSITORY_LEVEL]');
+                    throw new NotFoundErrror('Transaction record not found..');
+                } else {
+                    this.logs.info('Transaction record found...');
+                };
+
+                /* update record */
+                payment.status = status;
+
+                /* save update */
+                const updatedTransaction = await paymentRepo.save(payment);
+                return updatedTransaction;
             });
-
-            if (!payment) throw new NotFoundErrror('Transaction record not found..');
-
-            /* update record */
-            payment.status = status;
-
-            /* save update */
-            const updatedTransaction = await paymentRepo.save(payment);
-            return updatedTransaction;
-        });
+       } catch (e) {
+        this.logs.error(e, 'Error updating transaction status...');
+        throw e;
+       }
     }
 
     /* retrieve a transaction by its reference */
     async getTransactionByReference(reference: string): Promise<Transaction | null> {
-        const transaction = await this.paymentRepository.findOne({
-            where: { reference },
-        });
-        return transaction;
+        try {
+           const transaction = await this.paymentRepository.findOne({
+                where: { reference },
+            });
+            this.logs.info('Transaction fetched by reference...');
+        return transaction; 
+        } catch (e) {
+            this.logs.error(e, 'Error fetching transaction by reference...');
+            throw e;
+        };
     }
 
     /* retrieve transaction by id */
     async findById(id: string): Promise<Transaction | null> {
-        const transaction = await this.paymentRepository.findOne({
-            where: { id },
-        });
-        return transaction;
+        try {
+            const transaction = await this.paymentRepository.findOne({
+                where: { id },
+            });
+            return transaction;
+        } catch (e) {
+            this.logs.error(e, 'Error fetching transaction by id...');
+            throw e;
+        };
     }
 }
--- src/services/payment/paymentService.ts
+++ src/services/payment/paymentService.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
+import pino from 'pino';
 import { Configs } from '../../config/config';
 import { TransactionPayload, InitializeTransactionResponse } from '.';
-import { IPaymentRepository, ITransaction } from './paymentRepository';
-import { BadRequestError, NotFoundErrror } from '../../internal/error';
+import { IPaymentRepository } from './paymentRepository';
+import { BadRequestError } from '../../internal/error';
+import { Transaction } from '../../internal/store/entities/TransactionEntity';
 
 /* Payment Service Contract */
 export interface IPaymentService {
@@ -10,25 +12,28 @@ export interface IPaymentService {
   mountHeaders(): any;
   initializeTransaction(
     payload: TransactionPayload,
-  ): Promise<InitializeTransactionResponse>;
-  verifyTransaction(reference: string): Promise<any>;
+  ): Promise<Transaction>;
+  verifyTransaction(reference: string): Promise<Transaction>;
   handleWebhook(payload: any, signature: string): Promise<void>;
 }
 
 export class PaymentService implements IPaymentService {
   private paystackSecretKey: string;
   private paystackBaseUrl: string;
+  private logs: pino.Logger;
 
-  constructor(private readonly config: Configs, private readonly paymentRepository: IPaymentRepository) {
+  constructor(private readonly config: Configs, private readonly paymentRepository: IPaymentRepository, private logger: pino.Logger) {
     this.paystackSecretKey = config.paystack.paystackSecretKey;
     this.paystackBaseUrl = config.paystack.paystackBaseUrl;
+    this.logs = logger;
 
     if (!this.paystackSecretKey) {
+      this.logs.error('Missing Required config variable <paystack_secret_key: string>...[CATCH_PAYMENT_SERVICE_LEVEL]');
       throw new Error(
         'Missing Required config variable <paystack_secret_key: string>...',
       );
     }
-    console.log('✅ Payment Service is ready to make requests...');
+    this.logs.info('✅ Payment Service is ready to make requests...');
   }
 
   async getPaystackClient(): Promise<AxiosInstance> {
@@ -49,10 +54,13 @@ export class PaymentService implements IPaymentService {
 
   async initializeTransaction(
     payload: TransactionPayload,
-  ): Promise<InitializeTransactionResponse> {
+  ): Promise<Transaction> {
     try {
+      this.logs.info(payload, '⏳ Initializing payment...');
+
       const paystack = await this.getPaystackClient();
 
+      this.logs.debug('💬 Sending request to paystack API...');
       const response = await paystack.post('/transaction/initialize', {
         email: payload.email,
         amount: payload.amount * 100,
@@ -62,8 +70,15 @@ export class PaymentService implements IPaymentService {
       /* retrieve data from response */
       const responsePayload: InitializeTransactionResponse = response.data;
 
+      if (responsePayload.status === false) {
+        this.logs.error(responsePayload, '❌ Payment initialization failed...[CATCH_PAYMENT_SERVICE_LEVEL]');
+        throw new BadRequestError('API request did not work as planned...');
+      } else {
+        this.logs.info(responsePayload,'✅ Payment initialized successfully...');
+      };
+
       /* persist transaction details to database */
-      await this.paymentRepository.createTransaction({
+      const paymentIntent = await this.paymentRepository.createTransaction({
         amount: payload.amount,
         currency: payload.currency,
         email: payload.email,
@@ -71,27 +86,35 @@ export class PaymentService implements IPaymentService {
         reference: responsePayload.data.reference,
       });
       
-      console.log('✅ Payment initialized...');
-      return responsePayload;
+      this.logs.info(paymentIntent, '✅ Payment data saved successfully...');
+      return paymentIntent;
     } catch (e) {
-      throw new Error('Could not initialize payment');
+      this.logs.info(e, '❌ Payment initialization failed...');
+      throw new Error('Payment initialization failed...');
     };
   };
 
-  async verifyTransaction(reference: string): Promise<any> {
+  async verifyTransaction(reference: string): Promise<Transaction> {
     try {
+      this.logs.info({ reference }, '⏳ Attempting to verify transaction...');
       const paystack = await this.getPaystackClient();
 
       const response = await paystack.get('/transaction/verify/${reference}');
 
-      const { status } = response.data.data;
-      if (!status) throw new BadRequestError('API request did not work as planned...');
+      const body = response.data;
+      if (body.status === false) {
+        this.logs.error('❌ Transaction verification failed...[CATCH_PAYMENT_SERVICE_LEVEL]');
+        throw new BadRequestError('API request did not work as planned...');
+      };
+      this.logs.info(body, '✅ Transaction verified successfully...');
 
-      const updatedTransactionRecord = await this.paymentRepository.updateTransactionStatus(reference, status);
+      const updatedTransactionRecord = await this.paymentRepository.updateTransactionStatus(reference, body.status);
+      this.logs.info(updatedTransactionRecord, '✅ Transaction rcord successfully...');
       return updatedTransactionRecord;
     } catch (e) {
-      throw new Error('Error attempting to verify transaction');
-    }
+      this.logs.info(e, '❌ Transaction verification failed...');
+      throw new Error('Transaction verification failed...');
+    };
   };
 
   async handleWebhook(payload: any, signature: string): Promise<void> {
@@ -101,15 +124,18 @@ export class PaymentService implements IPaymentService {
       const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(payload)).digest('hex');
 
       if (hash !== signature) {
-        console.warn('Invalid webhook signature received');
-        throw new Error('Invalid signature');
-      }
+        this.logs.error('❌ Invalid webhook signature...[CATCH_PAYMENT_SERVICE_LEVEL]');
+        throw new Error('Invalid signature...');
+      };
+      this.logs.info('✅ Valid webhook signature...');
 
       const { event, data } = payload;
       if (event === 'charge.success') {
         await this.verifyTransaction(data.reference);
+        this.logs.info('✅ Webhook transaction processed successfully...');
       }
    } catch (e) {
+     this.logs.error(e, '❌ Error servicing webhook request...');
      throw new Error ('Error servicing webhook request...');
    }
   };
